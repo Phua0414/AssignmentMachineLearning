be40e88fe550d239ae6bb0670f9c2e7543f35a19
Phua0414 <phuahy-jm21@student.tarc.edu.my>
2025-04-24 23:58:32 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -218,7 +218,7 @@ def main():
             cluster_all = None
         
         if st.button("Run Clustering (Custom)"):
-             df_pca_dynamic, labels, silhouette, db_index, calinski_score, dunn_index_score = perform_dynamic_clustering(df_scaled, algorithm, k, eps, min_samples, damping, preference, n_components, bandwidth, bin_seeding, cluster_all)
+            df_pca_dynamic, labels, silhouette, db_index, calinski_score, dunn_index_score = perform_dynamic_clustering(df_scaled, algorithm, k, eps, min_samples, damping, preference, n_components, bandwidth, bin_seeding, cluster_all)
             st.write(f"### {algorithm} Clustering Results")
             st.write(f"Silhouette Score: {silhouette:.6f}")
             st.write(f"Davies-Bouldin Index: {db_index:.6f}")
